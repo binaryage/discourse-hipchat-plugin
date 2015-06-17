ac5354dd01555ec85299930f4e4b1f776f6901b7
Koinu <koinu@campfiretails.org>
2015-06-16 18:42:33 -0700

    Don’t send notifications about private messages
    Quick fix for binaryage/discourse-hipchat-plugin#1.

--- lib/hipchat.rb
+++ lib/hipchat.rb
@@ -30,24 +30,26 @@ module HipChat
 
   def self.report_event!(action, settings, user, topic, post=nil)
     begin
-      Rails.logger.info "Report event to HipChat: #{user} #{action} #{topic} #{post}"
-      category_markup = ""
-      category_markup = "[#{topic.category.name}] " if topic.category
-      user_markup = "<a href=\"#{Discourse.base_url}/users/#{user.username.downcase}\">#{user.username}</a>"
-      topic_markup = "topic: <a href=\"#{topic.url}\">#{topic.title}</a>"
+      unless topic.private_message?
+        Rails.logger.info "Report event to HipChat: #{user} #{action} #{topic} #{post}"
+        category_markup = ""
+        category_markup = "[#{topic.category.name}] " if topic.category
+        user_markup = "<a href=\"#{Discourse.base_url}/users/#{user.username.downcase}\">#{user.username}</a>"
+        topic_markup = "topic: <a href=\"#{topic.url}\">#{topic.title}</a>"
 
-      if action=="created-topic" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} started #{topic_markup}", settings
-      elsif action=="recovered-topic" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} recovered #{topic_markup}", settings
-      elsif action=="deleted-topic" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} deleted #{topic_markup}", settings
-      elsif action=="created-post" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} posted to #{topic_markup}", settings
-      elsif action=="deleted-post" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} deleted a post in #{topic_markup}", settings
-      elsif action=="recovered-post" then
-        send_message_to_hipchat! "#{category_markup}#{user_markup} recovered a post in #{topic_markup}", settings
+        if action=="created-topic" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} started #{topic_markup}", settings
+        elsif action=="recovered-topic" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} recovered #{topic_markup}", settings
+        elsif action=="deleted-topic" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} deleted #{topic_markup}", settings
+        elsif action=="created-post" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} posted to #{topic_markup}", settings
+        elsif action=="deleted-post" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} deleted a post in #{topic_markup}", settings
+        elsif action=="recovered-post" then
+          send_message_to_hipchat! "#{category_markup}#{user_markup} recovered a post in #{topic_markup}", settings
+        end
       end
     rescue => e
       Rails.logger.error e.message + "\n" + e.backtrace.join("\n")
